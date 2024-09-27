be6718f55d0bbf921bfb7726b30e774afdc35604
pRottinghuis <70864925+pRottinghuis@users.noreply.github.com>
2024-09-27 08:48:41 -0700

    Add logging warning to inform the user that their augmentation can't be created if the model file or target image size is too large.

--- src/cmd.py
+++ src/cmd.py
@@ -194,9 +194,11 @@ def augmentation(session, username: str, project_title: str, augmentation_title:
         # check if the model and target image files are too large before creating the augmentation
         if not ScARFileManager.save_and_size_check(session, '.glb'):
             session.logger.warning(f"Model file must be smaller than {APIManager.MAX_FILE_SIZE_MB}MB.")
+            session.logger.warning("Could not create new augmentation.")
             return
         if not ScARFileManager.save_and_size_check(session, '.png'):
             session.logger.warning(f"Target Image file must be smaller than {APIManager.MAX_FILE_SIZE_MB}MB.")
+            session.logger.warning("Could not create new augmentation.")
             return
 
         # The augmentation needs to be created on Schol-AR side.
